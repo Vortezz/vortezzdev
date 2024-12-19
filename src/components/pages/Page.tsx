3f9294f87267deab5820f8d58d2a4f70e9983d26
Vortezz <contact@vortezz.dev>
2024-12-19 18:43:20 +0100

    feat: improved timer and added end of timer

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,9 +1,14 @@
 import { Navbar } from "../sections/Navbar";
 import { Footer } from "../sections/Footer";
+import ReactConfetti from "react-confetti";
 
 export function Page({ children }: { children: JSX.Element }) {
+	const date = new Date();
+
 	return <div className={"leading-snug flex flex-col min-h-screen h-fit bg-white dark:bg-gray-833 scroll-smooth"}>
 		<Navbar />
+		{date.getDate() == 16 && date.getMonth() == 6 ?
+			<ReactConfetti></ReactConfetti> : <></>}
 		{children}
 		<Footer />
 	</div>;
--- src/pages/special/NewYearPage.tsx
+++ src/pages/special/NewYearPage.tsx
@@ -1,6 +1,7 @@
 import { Page } from "../../components/pages/Page";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import ReactConfetti from "react-confetti";
 
 export function NewYearPage() {
 	const targetTimestamp = 1735686000000;
@@ -30,7 +31,7 @@ export function NewYearPage() {
 	return <Page>
 		<div className={`py-24 w-full flex relative`}>
 			<Helmet>
-				<title>{`${days}d ${hours}h ${minutes}m ${seconds}s until 2025`}</title>
+				<title>{`${delta <= 0 ? "2025 is NOW!" : `${days}d ${hours}h ${minutes}m ${seconds}s until 2025`}`}</title>
 				<meta name="description"
 					content="Keep track of the time left before 2025!" />
 				<meta property="og:description"
@@ -38,34 +39,49 @@ export function NewYearPage() {
 				<meta property="twitter:description"
 					content="Keep track of the time left before 2025!" />
 			</Helmet>
-			<div className={"mx-auto w-full max-w-[calc(1536px)] flex flex-wrap justify-around flex-col 2xl:flex-row gap-y-16"}>
-				<div className={"m-auto flex flex-col"}>
-					<h1 className={"sm:text-[3rem] text-3xl text-primary-800 dark:text-white font-bold mx-auto leading-none"}>TIME BEFORE 2025:</h1>
-					<div className={"flex flex-row flex-wrap mt-8"}>
-						<div className={"flex flex-row flex-wrap mx-auto"}>
-							<div className={"flex flex-col mx-auto w-48 hover:-translate-y-1 transition-transform"}>
-								<h2 className={`text-[6rem] ${days == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>{("0" + days).slice(-2)}</h2>
-								<h2 className={`text-xl ${days == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>DAYS</h2>
+			{delta <= 0 ? <div className={"mx-auto w-full max-w-[calc(1536px)] flex flex-wrap justify-around flex-col 2xl:flex-row gap-y-16"}>
+					<div className={"m-auto flex flex-col"}>
+						<ReactConfetti></ReactConfetti>
+						<h1 className={"sm:text-[6rem] text-4xl text-primary-800 dark:text-white font-bold mx-auto leading-none"}>2025 IS NOW!</h1>
+						<h2 className={"sm:text-[3rem] text-2xl max-w-[80%] text-center mt-8 text-primary-800 dark:text-white font-bold mx-auto leading-none"}>Have a nice year everybody :)</h2>
+					</div>
+				</div>
+				:
+				<div className={"mx-auto w-full max-w-[calc(1536px)] flex flex-wrap justify-around flex-col 2xl:flex-row gap-y-16"}>
+					<div className={"m-auto flex flex-col"}>
+						<h1 className={"sm:text-[3rem] text-3xl text-primary-800 dark:text-white font-bold mx-auto leading-none"}>TIME BEFORE 2025:</h1>
+						<div className={"flex flex-row flex-wrap mt-8"}>
+							<div className={"flex flex-row flex-wrap mx-auto"}>
+								<div className={"flex flex-col mx-auto w-48 text-center hover:-translate-y-1 transition-transform"}>
+									<div className={`countdown text-[6rem] ${days == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>
+										<span style={{ "--value": ("0" + days).slice(-2) } as CSSProperties}></span>
+									</div>
+									<h2 className={`text-xl ${days == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>DAYS</h2>
+								</div>
+								<div className={"flex flex-col mx-auto w-48 text-center hover:-translate-y-1 transition-transform"}>
+									<div className={`countdown text-[6rem] ${days == 0 && hours == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>
+										<span style={{ "--value": ("0" + hours).slice(-2) } as CSSProperties}></span>
+									</div>
+									<h2 className={`text-xl ${days == 0 && hours == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>HOURS</h2>
+								</div>
 							</div>
-							<div className={"flex flex-col mx-auto w-48 hover:-translate-y-1 transition-transform"}>
-								<h2 className={`text-[6rem] ${days == 0 && hours == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>{("0" + hours).slice(-2)}</h2>
-								<h2 className={`text-xl ${days == 0 && hours == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>HOURS</h2>
-							</div>
-						</div>
-						<div className={"flex flex-row flex-wrap mx-auto"}>
-							<div className={"flex flex-col mx-auto w-48 hover:-translate-y-1 transition-transform"}>
-								<h2 className={`text-[6rem] ${days == 0 && hours == 0 && minutes == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>{("0" + minutes).slice(-2)}</h2>
-								<h2 className={`text-xl ${days == 0 && hours == 0 && minutes == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>MINUTES</h2>
-							</div>
-							<div className={"flex flex-col mx-auto w-48 hover:-translate-y-1 transition-transform"}>
-								<h2 className={`text-[6rem] ${days == 0 && hours == 0 && minutes == 0 && seconds == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>{("0" + seconds).slice(-2)}</h2>
-								<h2 className={`text-xl ${days == 0 && hours == 0 && minutes == 0 && seconds == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>SECONDS</h2>
+							<div className={"flex flex-row flex-wrap mx-auto"}>
+								<div className={"flex flex-col mx-auto w-48 text-center hover:-translate-y-1 transition-transform"}>
+									<div className={`countdown text-[6rem] ${days == 0 && hours == 0 && minutes == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>
+										<span style={{ "--value": ("0" + minutes).slice(-2) } as CSSProperties}></span>
+									</div>
+									<h2 className={`text-xl ${days == 0 && hours == 0 && minutes == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>MINUTES</h2>
+								</div>
+								<div className={"flex flex-col mx-auto text-center w-48 hover:-translate-y-1 transition-transform"}>
+									<div className={`countdown text-[6rem] ${days == 0 && hours == 0 && minutes == 0 && seconds == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} font-bold leading-none mx-auto`}>
+										<span style={{ "--value": ("0" + seconds).slice(-2) } as CSSProperties}></span>
+									</div>
+									<h2 className={`text-xl ${days == 0 && hours == 0 && minutes == 0 && seconds == 0 ? "text-primary-300" : "text-primary-800 dark:text-white"} mb-8 font-thin mx-auto`}>SECONDS</h2>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</div>}
 		</div>
-	</Page>
-		;
+	</Page>;
 }
